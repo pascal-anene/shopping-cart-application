15fa580c94d4b7dea551de9f09eb84f6e387a3e9
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-07 15:01:52 +0200

    completed first step, product object and products array

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,8 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 
+// Created products array to hold all product objects
+const products = [];
+
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -8,6 +11,30 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+const productCherry = {
+          name: "Cherry",
+          price: 4,
+          quantity: 0,
+          productId: 100,
+          image: "images/cherry.jpg"
+}
+
+const productOrange = {
+          name: "Orange",
+          price: 5,
+          quantity: 0,
+          productId: 101,
+          image: "images/orange.jpg"
+
+}
+
+const productStrawberry = {
+          name: "Strawberry",
+          price: 10,
+          quantity: 0,
+          productId: 102,
+          image: "images/strawberry.jpg"
+}
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
@@ -15,7 +42,13 @@
    - strawberry.jpg by Allec Gomes
 */
 
+// Adding each product object to the "products" array 
+products[0] = productCherry;
+products[1] = productOrange;
+products[2] = productStrawberry;
+
 /* Declare an empty array named cart to hold the items in the cart */
+const cart = [];
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
